910ade1ea209fe28a9edf0fa05a8e60e82dbdf67
Aswathy <aswathy.sreenath9@gmail.com>
2019-01-27 21:08:00 +0530

    Fixing socket connection error

--- src/main/java/api/Download.java
+++ src/main/java/api/Download.java
@@ -1,16 +1,31 @@
 package api;
 
+import io.restassured.RestAssured;
+import io.restassured.config.ConnectionConfig;
+import io.restassured.config.HttpClientConfig;
+import io.restassured.config.RestAssuredConfig;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
+import java.util.concurrent.TimeUnit;
+
 import static io.restassured.RestAssured.given;
+import static io.restassured.config.ConnectionConfig.connectionConfig;
 
 public class Download {
 
     public byte[] downloadPDF(String sessionToken,int id,int topicId,int userId) {
 
+        System.out.println("Worksheet ID: "+id);
+
         byte[] pdf = given()
-                .contentType(ContentType.JSON)
+
+                .config(RestAssured
+                        .config()
+                        .connectionConfig(new ConnectionConfig().
+                                closeIdleConnectionsAfterEachResponse())
+                        .httpClient(HttpClientConfig.httpClientConfig().reuseHttpClientInstance()))
+                .contentType("application/pdf")
                 .header("session_token",sessionToken)
                 .header("user_id",userId)
                 .pathParam("id",id)
