8bc24b9017493ec7b04c8133a1852e3bfe00b595
Aswathy <aswathy.sreenath9@gmail.com>
2019-01-30 09:00:36 +0530

    Fixing chunked connection close exception

--- src/main/java/api/Download.java
+++ src/main/java/api/Download.java
@@ -1,16 +1,8 @@
 package api;
 
-import io.restassured.RestAssured;
-import io.restassured.config.ConnectionConfig;
-import io.restassured.config.HttpClientConfig;
-import io.restassured.config.RestAssuredConfig;
 import io.restassured.http.ContentType;
-import io.restassured.response.Response;
-
-import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.config.ConnectionConfig.connectionConfig;
 
 public class Download {
 
@@ -19,13 +11,8 @@ public class Download {
         System.out.println("Worksheet ID: "+id);
 
         byte[] pdf = given()
+                .contentType(ContentType.ANY)
 
-                .config(RestAssured
-                        .config()
-                        .connectionConfig(new ConnectionConfig().
-                                closeIdleConnectionsAfterEachResponse())
-                        .httpClient(HttpClientConfig.httpClientConfig().reuseHttpClientInstance()))
-                .contentType("application/pdf")
                 .header("session_token",sessionToken)
                 .header("user_id",userId)
                 .pathParam("id",id)
